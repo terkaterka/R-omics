8c4c664331e3ad470085e3b9a28e7d497f125507
Teresa <teresa.domaszewska@gmail.com>
2015-07-22 19:44:25 +0200

    aaa

---
+++ R/hello.R
@@ -0,0 +1,17 @@
+#' Hello, world!
+#'
+#' This is a function just to show off with what I learned about R this week.
+#' It shows how much I'm happy that I'm coming back to Europe in comparison
+#' to what people estimate.
+#' It consists part of my 1st R package.
+#
+#' @author terka
+#' @description is not needed at this point
+#' @param x the number depicting how much you think I'm happy
+#' @return the real happiness
+#' @example f(5) = 1000*5 = 5000
+
+f <- function(x) {
+  x+1000
+}
+
